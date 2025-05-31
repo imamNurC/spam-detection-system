03feeb39c259e82179e51b7d930909d78d71c15f
ImamNurC <imamnc.developer@gmail.com>
2025-05-31 14:31:08 +0700

    add /whisper

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
+import os
+from flask_cors import CORS
 
 # Load the trained model and vectorizer
 loaded_model = joblib.load('email_spam_model_indo.pkl')
@@ -7,11 +9,35 @@ vectorizer = joblib.load('vectorizer.pkl')
 
 # Initialize the Flask app
 app = Flask(__name__)
+UPLOAD_FOLDER = "static/downloads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+CORS(app)  
 
 # Route to render the HTML page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/whisper', methods=['GET'])
+def index():
+    return render_template('whisper.html')
+
+# @app.route('/process', methods=['POST'])
+# def process():
+#     url = request.form['url']
+#     try:
+#         yt = YouTube(url)
+#         stream = yt.streams.filter(file_extension='mp4', progressive=True).order_by('resolution').desc().first()
+#         filename = f"{uuid.uuid4()}.mp4"
+#         filepath = os.path.join(UPLOAD_FOLDER, filename)
+#         stream.download(output_path=UPLOAD_FOLDER, filename=filename)
+#         # Transkripsi bisa dipanggil di sini
+#         transcription = "Transkripsi dummy untuk contoh."
+#         return render_template('index.html', video_path=f"downloads/{filename}", transcription=transcription)
+#     except Exception as e:
+#         return render_template('index.html', error=str(e))
+     
 
 # Route to handle the prediction
 @app.route('/predict', methods=['POST'])
@@ -34,6 +60,32 @@ def predict():
             return jsonify({"result": "Pesan Ini bukan spam"})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+
+
+@app.route('/process_video', methods=['POST'])
+def process_video():
+    # Ambil youtube_url dari body request (format JSON)
+    youtube_url = request.json.get('youtube_url')
+
+    # Cek apakah data youtube_url berhasil diterima
+    if youtube_url is None:
+        return jsonify({'error': 'youtube_url tidak ditemukan'}), 400
+
+    # Log atau print untuk memverifikasi
+    # print("Data yang diterima:", request.json)  
+    print("youtube_url:", youtube_url)         # Untuk melihat nilai youtube_url
+
+    # Proses URL YouTube (contoh placeholder untuk proses lebih lanjut)
+    transcription = process_youtube_video(youtube_url)
+
+    return jsonify({'transcription': transcription})
+
+def process_youtube_video(url):
+    # Placeholder untuk logika pengunduhan, konversi, dan transkripsi
+
+    return "Transkripsi hasil video dari: " + url
+
 
 if __name__ == '__main__':
     app.run(debug=True)
